82471090bf5a90946029b0e9c319668f807a1f6a
Alexander Alarcon <walarcon14@gmail.com>
2023-10-26 19:39:27 -0500

    feat(cli): Add validations for arguments/options

--- src/main.py
+++ src/main.py
@@ -1,12 +1,41 @@
+from typing import Optional
+
 import typer
 from typing_extensions import Annotated
 
+import validations as validations
+
 
 def main(
-    host: Annotated[str, typer.Argument(help="The host to scan for ports")],
-    ports: Annotated[str, typer.Option(help="The ports to scan")] = "1-1024",
-    num_threads: Annotated[int, typer.Option(help="Number of threads to use")] = 1,
+    host: Annotated[
+        str,
+        typer.Argument(
+            help="The host (IPv4 address) to scan for open ports.",
+            callback=validations.is_valid_ipv4_address,
+        ),
+    ],
+    ports: Annotated[
+        str,
+        typer.Option(
+            "--ports",
+            "-p",
+            help="The ports to scan, specified as a range (e.g., '1-80'), a single port (e.g., '80'), or a list of ports (e.g., '80,1024,3000').",
+            callback=validations.is_valid_range,
+        ),
+    ] = "1-1024",
+    num_threads: Annotated[
+        Optional[int],
+        typer.Option(
+            "--num-threads",
+            "-t",
+            help="Number of threads to use for the scan. A higher value may speed up the scan, but use it cautiously to avoid overloading your system. If not specified or number of threads are not available, the default will be calculated based on the available CPU cores.",
+            callback=validations.is_valid_threads,
+        ),
+    ] = None,
 ) -> None:
+    """
+    Scan a host for open ports.
+    """
     print(f"Scanning {host} for ports {ports} using {num_threads} threads.")
 
 
---
+++ src/validations.py
@@ -0,0 +1,110 @@
+import os
+import re
+import socket
+from typing import Optional
+
+from typer import BadParameter
+
+
+def is_valid_ipv4_address(address: str) -> str:
+    """
+    Validates if the given string is a valid IPv4 address.
+
+    Args:
+        address (str): The IP address to be validated.
+
+    Returns:
+        str: The validated IP address.
+
+    Raises:
+        BadParameter: If the given address is not a valid IPv4 address.
+    """
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except socket.error:
+        raise BadParameter(f"{address} is not a valid IPV4 address")
+    return address
+
+
+def is_valid_port(port: int) -> bool:
+    """
+    Check if the given port number is valid.
+
+    Args:
+        port (int): The port number to be validated.
+
+    Returns:
+        bool: True if the port number is valid, False otherwise.
+    """
+    return 1 <= port <= 65535
+
+
+def is_valid_range(range: str) -> str:
+    """
+    Check if the given range is valid.
+
+    Args:
+        range (str): The range to be validated.
+
+    Raises:
+        BadParameter: If the range format is invalid.
+
+    Returns:
+        None
+    """
+    pattern = r"^(((([1-9]\d*)|[1-9]+)-(([1-9]\d*)|[1-9]+))|(([1-9]\d*)|[1-9]+))(,(((([1-9]\d*)|[1-9]+)-(([1-9]\d*)|[1-9]+))|(([1-9]\d*)|[1-9]+))|(([1-9]\d*)|[1-9]+))*$"
+    if not re.match(
+        pattern,
+        range,
+    ):
+        raise BadParameter("Invalid range format.")
+
+    range_parts: list[str] = range.split(",")
+
+    for part in range_parts:
+        if "-" in part:
+            start, end = map(int, part.split("-"))
+            if not (is_valid_port(start) and is_valid_port(end)):
+                raise BadParameter(f"Invalid port range: {part}")
+        else:
+            port = int(part)
+            if not is_valid_port(port):
+                raise BadParameter(f"Invalid port number: {port}")
+
+    return range
+
+
+def is_valid_threads(user_specified_threads: Optional[int]) -> int:
+    """
+    Determine the number of threads to use for a task, taking into account the
+    user-specified thread count and the available CPU cores.
+
+    Args:
+        user_specified_threads (Optional[int]): The number of threads specified
+            by the user. If None, the function will use a default value.
+
+    Returns:
+        int: The number of threads to use, considering the user's preference and
+        the available CPU cores.
+
+    Note:
+        The function calculates the maximum number of threads based on the
+        available CPU cores and adds 4 to it, with a maximum limit of 32 threads.
+        If the user specifies a number of threads, it will be capped at the
+        maximum limit if it exceeds it.
+
+    """
+    available_cores: Optional[int] = os.cpu_count()
+
+    if available_cores is None:
+        print("CPU core count is not available. Using default thread count.")
+        num_threads = 4
+    else:
+        max_threads: int = min(32, available_cores + 4)
+
+        if user_specified_threads is None:
+            num_threads: int = max_threads
+        else:
+            num_threads = min(user_specified_threads, max_threads)
+
+    return num_threads
